2867ae126972e845c6a47485d32e5ea532591b71
animesh611 <www.ghosh.animesh1@gmail.com>
2023-04-07 10:22:41 +0530

    changes done 2nd1

--- src/test/java/org/example/BrowserTest.java
+++ src/test/java/org/example/BrowserTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,6 +9,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.util.ResourceBundle;
 
 public class BrowserTest {
     WebDriver driver;
@@ -27,16 +30,15 @@ public class BrowserTest {
         driver.get(link);
         driver.manage().window().maximize();
     }
-        @Test
-
-        public void googleTest() throws InterruptedException {
-
-
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//input[@id=\"identifierId\"]")).sendKeys("www.mail@gmail.com");
-
-        driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
-        Thread.sleep(1000);
+    @Test
+    public void login() throws InterruptedException {
+        ResourceBundle r=ResourceBundle.getBundle("config");
+        String username=r.getString("username");
+        driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys(username);
+        driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+        Thread.sleep(5000);
+        Assert.assertEquals(driver.getTitle(), "Gmail");
+    }
 //        driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("abc");
 //        driver.findElement(By.name("password")).sendKeys("secret_sauce");
     }

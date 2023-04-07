081ce792b229c14888808d47881de8c8a116dfa4
animesh611 <www.ghosh.animesh1@gmail.com>
2023-04-07 10:59:26 +0530

    changes final

--- src/test/java/org/example/BrowserTest.java
+++ src/test/java/org/example/BrowserTest.java
@@ -1,11 +1,12 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -14,21 +15,22 @@ import java.util.ResourceBundle;
 
 public class BrowserTest {
     WebDriver driver;
-
     @BeforeClass
-    @Parameters({"browser", "url"})
-    void setup(String browser, String link) {
-        if (browser.equalsIgnoreCase("chrome")) {
+    @Parameters({"browser","url"})
+    void setup(String browser,String link)
+    {
+        if(browser.equalsIgnoreCase("chrome")){
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver=new ChromeDriver();
         }
         else if (browser.equalsIgnoreCase("edge")) {
             WebDriverManager.edgedriver().setup();
-            driver=new EdgeDriver();}
-
+            driver=new EdgeDriver();
+        }
 
         driver.get(link);
         driver.manage().window().maximize();
+
     }
     @Test
     public void login() throws InterruptedException {
@@ -39,7 +41,9 @@ public class BrowserTest {
         Thread.sleep(5000);
         Assert.assertEquals(driver.getTitle(), "Gmail");
     }
-//        driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("abc");
-//        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+    @AfterClass
+    void close(){
+
+        driver.close();
     }
 }
